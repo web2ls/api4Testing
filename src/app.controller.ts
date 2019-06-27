96c8aaaf795567860d63d3a58b4aeeef496bf02c
web2ls <web2ls@yandex.ru>
2019-06-27 20:21:55 +0300

    added static distribution

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Res, HttpStatus, NotFoundException } from '@nestjs/common';
 import { join } from 'path';
 import * as fs from 'fs';
+import { getUnpackedSettings } from 'http2';
 
 const PATH_TO_STATIC_DATA = '/public/files/static_data.json';
 
-@Controller('api')
+@Controller()
 export class AppController {
     constructor() { }
+
+    @Get()
+    getInterface(@Res() res) {
+        res.status(HttpStatus.OK).sendFile(join(__dirname, "..", "frontend", "index.html"));
+    }
 
     @Get('/static-data')
     getStaticData(@Res() res) {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  // const app = await NestFactory.create(AppModule);
+  app.useStaticAssets(join(__dirname, '..', "frontend"));
   await app.listen(3000);
 }
 bootstrap();
